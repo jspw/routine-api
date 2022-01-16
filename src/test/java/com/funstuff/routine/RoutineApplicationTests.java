2224f26141eede990b923536f3f04eb44facce67
jspw <mhshifat757@gmail.com>
2022-01-16 09:42:46 +0600

    init project

---
+++ src/test/java/com/funstuff/routine/RoutineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.funstuff.routine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoutineApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
